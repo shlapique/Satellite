90c81ab63cce98646ae26332199dac65c6e0e823
shlapique <gordsdima@gmail.com>
2022-01-04 20:13:53 +0300

    начало набросал

---
+++ satellite/satellite/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(155, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(424, 317);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(315, 157);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 99);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(673, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ satellite/satellite/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Point loc1; //loc picture box1
+        private Point loc_earth; // location of Earth
+
+        private Point center; // center 
+
+        private Graphics g;
+        private Pen pen;
+
+        double angle1 = 0;
+
+
+        // Create solid brush.
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            loc1 = new Point((ClientSize.Width / 2) - (pictureBox1.Width / 2), (ClientSize.Height / 2) - (pictureBox1.Height / 2));
+            loc_earth = new Point(loc1.X + (pictureBox1.Width / 2) - (pictureBox2.Width / 2), loc1.Y + (pictureBox1.Height / 2) - (pictureBox2.Height / 2));
+            center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            pictureBox1.Location = loc1;
+            pictureBox2.Location = loc_earth;
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x, y;
+            //////////////////////////////////
+            x = (int)(center.X + 150 * Math.Cos(angle1));
+            y = (int)(center.Y + 150 * Math.Sin(angle1));
+
+            Point loc = new Point(x, y);
+
+            Rectangle rect = new Rectangle(loc, new Size(10, 10));
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            // Fill ellipse on screen.
+            g.FillEllipse(redBrush, rect);
+
+            pictureBox1.Image = bmp;
+
+
+            //Rectangle sat = new Rectangle();
+
+            angle1 -= 0.00826; Task.Delay(1);
+        }
+
+
+
+        public class Earth
+        {
+            public int orbit_count = 1;
+
+
+
+        }
+
+        private class Orbit : Earth
+        {
+            public Orbit() { }
+            private int r = 100;
+
+            public int a { get { return r; } set { r = value; } }
+
+        }
+
+        private class Sat : Orbit
+        {
+            //Rectangle sat = new Rectangle();
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Orbit orbit = new Orbit();
+            int r = orbit.a;
+            //g = this.CreateGraphics();
+            pen = new Pen(Color.Black); // Нашему карандашу присваиваем зеленый цвет
+
+            if (true)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                {
+                    //g.Clear(Color.White);
+                    g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - r, center.Y + r, center.X + r, center.Y - r)); //Рисует эллипс
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer x = new Timer();
+            x.Interval = 1;
+            x.Start();
+            x.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
