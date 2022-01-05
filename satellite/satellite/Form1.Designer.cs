c61bc6d86397f561acd96f6a58ff6b5c93634aa9
shlapique <gordsdima@gmail.com>
2022-01-05 18:55:08 +0300

    update

--- satellite/satellite/Form1.Designer.cs
+++ satellite/satellite/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -50,8 +50,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(315, 157);
+            this.pictureBox2.Location = new System.Drawing.Point(23, 20);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(108, 99);
             this.pictureBox2.TabIndex = 1;
@@ -76,11 +75,21 @@
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(661, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
@@ -91,6 +100,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,6 +111,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- satellite/satellite/Form1.cs
+++ satellite/satellite/Form1.cs
@@ -16,14 +16,27 @@ namespace WindowsFormsApp1
         private Point loc_earth; // location of Earth
         private Point center; // center 
 
-        double angle, angle1 = 0;
+        private Point center_new; // center_new
+
+        private Point center_new2;
+
+        double angle, angle1, angle2 = 0;
 
 
         // Create solid brush.
         SolidBrush redBrush = new SolidBrush(Color.Red);
         Pen pen = new Pen(Color.Black);
         Bitmap bm;
+
+        Bitmap bm2;
+
         Graphics g;
+
+        Graphics g2;
+
+        Image im;
+
+        Point tmpPonit = new Point();
 
         public Form1()
         {
@@ -31,12 +44,23 @@ namespace WindowsFormsApp1
             bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(bm);
 
+            im = Image.FromFile("H:\\projects\\mai\\isrpps\\cursed\\img\\earth.jpg");
+
+
+            bm2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            g2 = Graphics.FromImage(bm2);
+
             this.Size = new Size(800, 450);
             loc1 = new Point((ClientSize.Width / 2) - (pictureBox1.Width / 2), (ClientSize.Height / 2) - (pictureBox1.Height / 2));
             loc_earth = new Point(loc1.X + (pictureBox1.Width / 2) - (pictureBox2.Width / 2), loc1.Y + (pictureBox1.Height / 2) - (pictureBox2.Height / 2));
             center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            center_new = new Point(pictureBox1.Width / 2 - 50, pictureBox1.Height / 2 - 50);
+
+            //center_new2 = new Point()
+
             pictureBox1.Location = loc1;
-            pictureBox2.Location = loc_earth;
+            //pictureBox2.Location = loc_earth;
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -44,27 +68,86 @@ namespace WindowsFormsApp1
 
             int x, y;
 
-            x = (int)(center.X + 150 * Math.Cos(angle));
-            y = (int)(center.Y + 150 * Math.Sin(angle));
+            x = (int)(center.X + 20 * Math.Cos(angle));
+            y = (int)(center.Y + 160 * Math.Sin(angle));
+
             Point loc = new Point(x - 7, y - 7);
-            Rectangle sat = new Rectangle(loc, new Size(14, 14));
-            // Fill ellipse on screen.
-            g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 150, center.Y + 150, center.X + 150, center.Y - 150)); //Рисует эллипс
-            g.FillEllipse(redBrush, sat);
+
+            double angleStart = Math.Acos(20.0 / 50.0);
+            angleStart = angleStart * 180.0 / Math.PI;
+
+            double b = Math.Abs(loc.X - center.X); // b
+
+            double tmp_angle = Math.Acos(b / 50.0);
+            tmp_angle = tmp_angle * 180.0 / Math.PI;
+
+            if (tmpPonit.Y >= loc.Y & (tmp_angle <= angleStart || tmp_angle >= 360 - angleStart * 2.0))
+            {
+
+                Rectangle sat = new Rectangle(loc, new Size(14, 14));
+                // Fill ellipse on screen.
+                //g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 20, center.Y + 160, center.X + 20, center.Y - 160)); //Рисует эллипс
+
+
+                g.FillEllipse(redBrush, sat);
+
+                g.DrawImage(im, center_new);
+
+                g.DrawArc(pen, (float)center.X - 20, (float)center.Y - 160, 40, 320, (float)angleStart, 360 - (float)angleStart * 2);
+            }
+            else
+            {      
+                //означает, что точка двигается вниз = > точка и ось должна быть выше пикчи Земли
+                g.DrawImage(im, center_new);
+
+                Rectangle sat = new Rectangle(loc, new Size(14, 14));
+                // Fill ellipse on screen.
+                //g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 20, center.Y + 160, center.X + 20, center.Y - 160)); //Рисует эллипс
+
+
+                //g.DrawArc(pen, (float)center.X - 20, (float)center.Y - 160, 40, 320, 90, 220);
+                g.DrawArc(pen, (float)center.X - 20, (float)center.Y - 160, 40, 320, (float)angleStart, 360 - (float)angleStart * 2);
+
+                g.FillEllipse(redBrush, sat);
+
+            }
+            tmpPonit = loc;
+
+            /*
+
             /////////////////////////////////
             int x1, y1;
 
-            x1 = (int)(center.X + 60 * Math.Cos(angle));
+            g.DrawImage(im, loc_earth);
+
+            x1 = (int)(center.X + 120 * Math.Cos(angle));
             y1 = (int)(center.Y + 150 * Math.Sin(angle));
             Point loc1 = new Point(x1 - 7, y1 - 7);
             Rectangle sat1 = new Rectangle(loc1, new Size(14, 14));
             // Fill ellipse on screen.
-            g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 60, center.Y + 150, center.X + 60, center.Y - 150)); //Рисует эллипс
+            g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 120, center.Y + 150, center.X + 120, center.Y - 150)); //Рисует эллипс
             g.FillEllipse(redBrush, sat1);
+            /////////////////////////////////
+            int x2, y2;
 
+            x2 = (int)(center.X + 20* Math.Cos(angle));
+            y2 = (int)(center.Y + 160 * Math.Sin(angle));
+            Point loc2 = new Point(x2 - 7, y2 - 7);
+            Rectangle sat2 = new Rectangle(loc2, new Size(14, 14));
+            // Fill ellipse on screen.
+
+           
+
+            g.DrawEllipse(pen, Rectangle.FromLTRB(center.X - 20, center.Y + 160, center.X + 20, center.Y - 160)); //Рисует эллипс
+            g.FillEllipse(redBrush, sat2);
+
+           */
 
             pictureBox1.Image = bm;
-            angle -= 0.0223; Task.Delay(1);
+
+            label1.Text = "X: " + loc.X + " Y: " + loc.Y + "\n  angle: " + angleStart + " \n angle_tmp: " + tmp_angle + " \n center: " + center.X + "," + center.Y;
+
+            angle -= 0.0123; Task.Delay(1);
         }
 
 
